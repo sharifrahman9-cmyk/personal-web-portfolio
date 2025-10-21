aedd246f8fa4d324d6e1739dc2d4a54fc769cd3c
sharif <sharifrahman9@gmail.com>
2025-10-21 16:07:30 +0600

    ssh test

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
+const Page = () => {
+  return(
     <div>
-      <h1>Welcome to My Personal Web Portfolio</h1>
-      <p>
-        This is the homepage of my personal web portfolio built with Next.js.
-      </p>
+
     </div>
-  );
-}
+  ); 
+  
+  export default Page
